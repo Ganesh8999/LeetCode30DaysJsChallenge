7515e22ca317a8c6297a20f734a184f6a2c05e45
Ganesh8999 <singhganesh571@gmail.com>
2024-11-08 21:57:32 +0530

    reduce array implementation

---
+++ 6_Day/reduce.js
@@ -0,0 +1,36 @@
+/* 2626. Array Reduce Transformation
+Easy d 455 4 23 • Add to List I Share
+Given an integer array nums, a reducer function fn, and an initial value init, return a reduced array.
+A reduced array is created by applying the following operation: val = fn (init, nums [0]), val = fn(val,
+nums [1]), val = fn(val, nums (2]), •.. until every element in the array has been processed. The final value of val is returned.
+If the length of the array is 0, it should return init.
+Please solve it without using the built-in Array. reduce method.
+Example 1:
+Input:
+nums = [1,2,3,4]
+fn = function sum (accum, curr) { return accum + curr; }
+init = 0
+Output: 10
+Explanation:
+initially, the value is init=0.
+(0) + nums [0] = 1
+(1) + nums [11 = 3
+(3) + nums [2] = 6
+(6) + nums [3] = 10
+The final answer is 10. */
+
+const reduce = function (arr, fn, init) {
+  let accValue = init;
+  arr.forEach((element) => (accValue = fn(accValue, element)));
+  return accValue;
+};
+
+console.log(
+  reduce(
+    [1, 2, 3],k
+    function sum(accum, curr) {
+      return accum + curr;
+    },
+    0
+  )
+);
